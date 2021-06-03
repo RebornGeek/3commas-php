a11df903996f5bd2d3e848227abe1866e2e7090f
Sam Hemingway <thereborngeek@gmail.com>
2021-06-03 16:33:06 -0700

    MASTER Initial commit

---
+++ 3commas-php.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *
+ */
+
+class threeCommasApi {
+    public function __construct() {
+        $this->apiKey = ''; // Enter apiKey here
+        $this->secretKey = ''; // Enter secretKey here
+    }
+
+    private function requestJson() {
+        
+    }
+}
